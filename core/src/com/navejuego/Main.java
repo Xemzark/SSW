b2efd7cc71b99c1d3dac8a0c1fff8143c72e4604
Elijah <eliasmaidanik@gmail.com>
2016-05-04 13:35:40 +0200

    First time preferences/save

--- core/src/com/navejuego/Main.java
+++ core/src/com/navejuego/Main.java
@@ -20,6 +20,17 @@ public class Main extends Game {
 		Constantes.CalculateScreenConstants();
 		ScreenManager.getInstance().initialize(this); //Inicializo el AssetManager
 		GestorAssets.getInstance().create();
+
+		if(PartidaGuardada.getInstance().firstTime()){
+			PartidaGuardada.getInstance().fillPutuaciones();
+			PartidaGuardada.getInstance().setFirstime(false);
+			Preferencias.getInstance().setMusic(true);
+			Preferencias.getInstance().setSound(true);
+			Preferencias.getInstance().setVibration(true);
+			PartidaGuardada.getInstance().saveGameData();
+			Preferencias.getInstance().savePreferences();
+		}
+
 		ScreenManager.getInstance().showScreen(ScreenEnum.MAIN_MENU);
 	}
 }
