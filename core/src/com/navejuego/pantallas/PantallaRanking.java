e50a87e51f8c7a3765026222f5cb28772e3f4fb3
Roger <roger@androidlista.com>
2016-05-07 20:49:33 +0200

    Añadida funcionalidad para pasar entre rankings de niveles, falta hacerlo relativo a pantalla y cambiar el sprite de la flecha de la derecha.

--- core/src/com/navejuego/pantallas/PantallaRanking.java
+++ core/src/com/navejuego/pantallas/PantallaRanking.java
@@ -6,18 +6,26 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.navejuego.GestorAssets;
 import com.navejuego.PartidaGuardada;
+
+import java.awt.Image;
+
+import javax.xml.soap.Text;
 
 /**
  * Created by albertmoreno on 14/4/16.
@@ -29,6 +37,7 @@ public class PantallaRanking extends Pantalla {
     private Stage rankingStage;
 
 
+    private Label title;
     private Skin skinRanking;
     private Texture backgroundRanking;
     private BitmapFont font;
@@ -40,7 +49,7 @@ public class PantallaRanking extends Pantalla {
 
     int[] Posiciones = new int[PartidaGuardada.getInstance().getMaxPuntuaciones()];
     int[] Puntuaciones = new int[PartidaGuardada.getInstance().getMaxPuntuaciones()];
-    int nivelACargar = 0;
+    int nivelACargar=0;
 
     Table scrollTable = new Table();
 
@@ -57,6 +66,8 @@ public class PantallaRanking extends Pantalla {
     public void setNivelACargar(int nivel){
 
         this.nivelACargar = nivel;
+        System.out.println("Cargando nivel "+nivel);
+        this.Puntuaciones=new int[PartidaGuardada.getInstance().getMaxPuntuaciones()];
         this.Puntuaciones = PartidaGuardada.getInstance().getPuntuaciones(this.nivelACargar);
 
         for(int i = 1; i != PartidaGuardada.getInstance().getMaxPuntuaciones() + 1; i++){
@@ -69,7 +80,7 @@ public class PantallaRanking extends Pantalla {
      * rellena el scroll con las puntuaciones indicadas en SetNivelACargar(nivel);
      */
     public void fillScrollTable(){
-
+        scrollTable.clear();
         this.labelstyle.font= font;
 
         for(int i=0; i<Posiciones.length;i++) {
@@ -97,15 +108,65 @@ public class PantallaRanking extends Pantalla {
         Gdx.input.setInputProcessor(rankingStage);
         skinRanking = new Skin(Gdx.files.internal("skin/uiskin.json"));
 
+
+        PartidaGuardada.getInstance().setPuntucion(1,8888);
+
+
         font = new BitmapFont(Gdx.files.internal("otherfont/font.fnt"));
+        Label.LabelStyle ls = new Label.LabelStyle(font, null);
+
         scrollTable.row();
         final ScrollPane scroller = new ScrollPane(scrollTable);
         scroller.setSize(scrollerSizeX, scrollerSizeY);
         scroller.setPosition(0, 0.25f * Gdx.graphics.getHeight());
         rankingStage.addActor(scroller);
 
+        title = new Label("Nivel "+(nivelACargar+1),ls);
+        title.setPosition(0.27f * Gdx.graphics.getWidth(), 0.58f * Gdx.graphics.getHeight());
+        title.setFontScale(0.5f);
+        title.setSize(title.getWidth() * title.getFontScaleX(), title.getHeight() * title.getFontScaleY());
+
+        ImageButton leftarrow=new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("otherskin/left-pointing-arrow.png"))));
+        leftarrow.setSize(50,50);
+        leftarrow.setPosition(0, 0);
+
+        ImageButton rightarrow = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture("otherskin/left-pointing-arrow.png"))));
+        rightarrow.setSize(50,50);
+        rightarrow.setPosition(Gdx.graphics.getWidth() - 128, 0);
+
+        leftarrow.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if (nivelACargar > 0) {
+                    nivelACargar -= 1;
+                    setNivelACargar(nivelACargar);
+                    fillScrollTable();
+                    title.setText("Nivel "+(nivelACargar+1));
+
+                }
+            }
+        });
+
+        rightarrow.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if(nivelACargar < PartidaGuardada.getInstance().getMaxNiveles()-1) {
+                    nivelACargar += 1;
+                    setNivelACargar(nivelACargar);
+                    fillScrollTable();
+                    title.setText("Nivel "+(nivelACargar+1));
+                }
+            }
+        });
+
+
+        rankingStage.addActor(title);
+        rankingStage.addActor(rightarrow);
+        rankingStage.addActor(leftarrow);
+
+
         //Definir nivel del cual cargar puntuaciones
-        this.setNivelACargar(0);
+        this.setNivelACargar(nivelACargar);
         //Rellenar el scroll con las puntuaciones
         this.fillScrollTable();
 
