82dd9304bd6001b14400a879cd922ff5d9aebc0c
alejandrogarsan <beno_dk@hotmail.com>
2016-05-18 21:57:51 +0200

    Comentarios 3

--- core/src/com/navejuego/AnimacionChain.java
+++ core/src/com/navejuego/AnimacionChain.java
@@ -3,23 +3,33 @@ package com.navejuego;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
-
 import java.util.ArrayList;
 
 /**
- * Created by Kevin on 29/04/2016.
+ * Clase que ejecuta cualquier animación derivada de la clase Animación
  */
 public class AnimacionChain extends Animacion {
     private int times; //Veces que se repite la animacion
     private int indiceTotal = 0;
     private Sound sound;
 
+    /**
+     * Constructor
+     * @param texture Array de texturas que componen la animación
+     * @param posicion Posicion donde debe ejecutarse la animación
+     * @param duracion Duranción de la animación
+     * @param size Tamaño de los sprites de la animación
+     */
     public AnimacionChain(ArrayList<Texture> texture, Vector2 posicion, float duracion, int times, float size) {
         super(texture, posicion, duracion, size);
         this.times = times;
-        this.sound = sound;
     }
 
+    /**
+     * Selecciona la siguiente textura de la animación que será dibujada. I si se ha
+     * terminado la animacion esta se destruye
+     * @param delta
+     */
     @Override
     public void act(float delta) {
 
@@ -36,7 +46,6 @@ public class AnimacionChain extends Animacion {
 
         }
         else{
-            //Desbloquear siguiente nivel
             destruirse();
         }
     }
